bd3b46dce1affe4969c7e6552dfadb5909c69f19
Travis Rennemann <rennemannt@gmail.com>
2020-06-17 23:50:13 -0400

    reorganized files and added colors

---
+++ wall-e/server/webserver.js
@@ -0,0 +1,17 @@
+/**
+ * Node.js Fastify server.
+ */
+
+const fastify = require('fastify')({
+    logger: true
+})
+
+fastify.register(require('./routes'))
+
+fastify.listen(3000, function (err, address) {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+    fastify.log.info(`server listening on ${address}`)
+})
